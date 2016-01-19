b05cbc0fefd2164201859a6e7cff5871bfccd82b
Briana Chapman <bri.chapman@gmail.com>
2016-01-19 17:41:05 -0600

    Initial Commit

---
+++ InfiniteMarimba/InfiniteMarimba.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteMarimba.h
+//  InfiniteMarimba
+//
+//  Created by Bri Chapman on 19/01/16.
+//  Copyright © 2016 com.bchapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteMarimba.
+FOUNDATION_EXPORT double InfiniteMarimbaVersionNumber;
+
+//! Project version string for InfiniteMarimba.
+FOUNDATION_EXPORT const unsigned char InfiniteMarimbaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteMarimba/PublicHeader.h>
+
+
